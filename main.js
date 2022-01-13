93caf408c013232a48a356d8ff469ca881a3cacc
Alireza Nasseh <alireza.nasseh@gmail.com>
2022-01-14 01:04:06 +0330

    Completed move logic, added keyboard control, added check table, added randomize table

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let emptyCell = 25;
+let emptyPos = {x: 4, y: 4}, pos = {}, num = {};
 
 function rootStyles(root) {
     root.style.height = '100vh';
@@ -18,12 +18,12 @@ function tableStyles(table) {
     table.style.position = 'relative';
 }
 
-function pieceStyles(piece, i, j) {
+function pieceStyles(piece, x, y) {
     piece.style.border = '2px solid rgb(87 87 203)';
     piece.style.backgroundColor = 'rgb(120,120,238)';
     piece.style.position = 'absolute';
-    piece.style.top = (i * 80 + 2).toString() + 'px';
-    piece.style.left = (j * 80 + 2).toString() + 'px';
+    piece.style.top = (y * 80 + 2).toString() + 'px';
+    piece.style.left = (x * 80 + 2).toString() + 'px';
     piece.style.width = '74px';
     piece.style.height = '74px';
     piece.style.display = 'flex';
@@ -32,11 +32,12 @@ function pieceStyles(piece, i, j) {
 }
 
 function pieceLinks() {
-    let piece;
+    let piece, cellPos;
     for (let i = 1; i < 25; i++) {
         piece = document.getElementById(i.toString());
         if (piece) {
-            if (i % 5 === emptyCell % 5 || Math.floor(i / 5) === Math.floor((emptyCell - 1) / 5)) {
+            cellPos = pos[i];
+            if (cellPos.x === emptyPos.x || cellPos.y === emptyPos.y) {
                 piece.style.cursor = 'pointer';
             } else {
                 piece.style.cursor = 'default';
@@ -45,33 +46,116 @@ function pieceLinks() {
     }
 }
 
-function move(id) {
-    id = parseInt(id);
-    if (id % 5 === emptyCell % 5) {
-        let start, end, add;
-        if (id < emptyCell) {
-            start = id;
-            end = emptyCell;
-            add = 80;
-        } else {
-            start = emptyCell;
-            end = id;
-            add = -80;
+function piecePositions() {
+    for (let i = 1; i < 25; i++) {
+        piece = document.getElementById(i.toString());
+        if (piece) {
+            piece.style.top = (pos[i].y * 80 + 2).toString() + 'px';
+            piece.style.left = (pos[i].x * 80 + 2).toString() + 'px';
         }
-        for (let i = start; i < end; i += 5) {
-            const piece = document.getElementById(i.toString());
-            if (piece) {
-                const topStr = piece.style.top;
-                const topNumber = parseInt(topStr.substr(0, topStr.length - 2));
-                piece.style.top = (topNumber + add).toString() + 'px';
+    }
+}
+
+function checkTable() {
+    let solved = true;
+    let number = 1;
+    for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < 5; x++) {
+            if (num[x][y] !== number++) {
+                solved = false;
+                break;
             }
         }
-        emptyCell = id;
+        if (!solved) break;
+    }
+    if (solved) {
+        party.confetti(table, {
+            count: party.variation.range(100, 200),
+            shapes: ['star']
+        });
+    }
+}
+
+function move(id, byUser = true) {
+    const cellPos = pos[id];
+    if (cellPos.x === emptyPos.x) {
+        let x = cellPos.x;
+        let number;
+        if (cellPos.y < emptyPos.y) {
+            for (let y = emptyPos.y; y > cellPos.y; y--) {
+                number = num[x][y - 1];
+                num[x][y] = number;
+                pos[number] = {x, y};
+            }
+        } else if (cellPos.y > emptyPos.y) {
+            for (let y = emptyPos.y; y < cellPos.y; y++) {
+                number = num[x][y + 1];
+                num[x][y] = number;
+                pos[number] = {x, y};
+            }
+        }
+        emptyPos.y = cellPos.y;
+        num[x][emptyPos.y] = 25;
+        pos[25] = {x, y: emptyPos.y};
+        piecePositions();
         pieceLinks();
-    } else if (Math.floor(id / 5) === Math.floor((emptyCell - 1) / 5)) {
-        console.log('row');
-        const distance = emptyCell - id;
-        console.log(distance)
+        if (byUser) checkTable();
+    } else if (cellPos.y === emptyPos.y) {
+        let y = cellPos.y;
+        let number;
+        if (cellPos.x < emptyPos.x) {
+            for (let x = emptyPos.x; x > cellPos.x; x--) {
+                number = num[x - 1][y];
+                num[x][y] = number;
+                pos[number] = {x, y};
+            }
+        } else if (cellPos.x > emptyPos.x) {
+            for (let x = emptyPos.x; x < cellPos.x; x++) {
+                number = num[x + 1][y];
+                num[x][y] = number;
+                pos[number] = {x, y};
+            }
+        }
+        emptyPos.x = cellPos.x;
+        num[emptyPos.x][y] = 25;
+        pos[25] = {x: emptyPos.x, y};
+        piecePositions();
+        pieceLinks();
+        if (byUser) checkTable();
+    }
+}
+
+function keyMove(event, byUser = true) {
+    switch (event.code) {
+        case 'ArrowUp':
+            if (emptyPos.y < 4) {
+                move(num[emptyPos.x][emptyPos.y + 1], byUser);
+            }
+            break;
+        case 'ArrowDown':
+            if (emptyPos.y > 0) {
+                move(num[emptyPos.x][emptyPos.y - 1], byUser);
+            }
+            break;
+        case 'ArrowLeft':
+            if (emptyPos.x < 4) {
+                move(num[emptyPos.x + 1][emptyPos.y], byUser);
+            }
+            break;
+        case 'ArrowRight':
+            if (emptyPos.x > 0) {
+                move(num[emptyPos.x - 1][emptyPos.y], byUser);
+            }
+            break;
+    }
+}
+
+function randomize(n) {
+    const codes = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'];
+    let rnd;
+    for (let i = 0; i < n; i++) {
+        rnd = Math.floor(Math.random() * 4);
+        keyMove({code: codes[rnd]}, false);
     }
 }
 
@@ -79,20 +163,29 @@ const root = document.createElement("div");
 rootStyles(root);
 const table = document.createElement('div');
 tableStyles(table);
-let piece;
-for (let i = 0; i < 5; i++) {
-    for (let j = 0; j < 5; j++) {
-        if (i < 4 || j < 4) {
+let piece, number;
+for (let y = 0; y < 5; y++) {
+    for (let x = 0; x < 5; x++) {
+        number = (x + 1) + (y * 5);
+        if (x < 4 || y < 4) {
             piece = document.createElement('div');
-            pieceStyles(piece, i, j);
-            piece.id = piece.innerText = ((j + 1) + (i * 5)).toString();
+            pieceStyles(piece, x, y);
+            piece.id = piece.innerText = number.toString();
             piece.onclick = function () {
                 move(this.id);
             };
             table.appendChild(piece);
         }
+        pos[number.toString()] = {x, y};
+        if (num[x]) {
+            num[x][y] = number;
+        } else {
+            num[x] = [number];
+        }
     }
 }
 root.appendChild(table);
 document.body.appendChild(root);
+document.addEventListener('keydown', keyMove);
+randomize(100);
 pieceLinks();
